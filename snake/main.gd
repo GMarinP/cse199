39f0383715bffbbccc1e99d27b0f63a11546cf29
GMarinP <giovannamarinpereira@gmail.com>
2026-05-14 15:15:51 -0600

    EVERYTHING BROKE

--- snake/main.gd
+++ snake/main.gd
@@ -8,6 +8,7 @@ var grid_height = 20
 var cell_size = 32
 
 @onready var head = get_node("snakeHead")
+@onready var tilemap = $TileMap   # your wall TileMap
 
 func _ready():
 	spawn_food()
@@ -15,26 +16,24 @@ func _ready():
 func spawn_food():
 	if current_food:
 		current_food.queue_free()
-
+		
 	current_food = FoodScene.instantiate()
-
+	
 	var position_ok = false
 	var new_pos = Vector2.ZERO
-
+	
 	while not position_ok:
 		var x = randi() % grid_width
 		var y = randi() % grid_height
-		new_pos = Vector2(x, y) * cell_size
-
-		position_ok = true
-		for wall in get_tree().get_nodes_in_group("wall"):
-			if wall.position == new_pos:
-				position_ok = false
-				break
-
+		var cell = Vector2i(x, y)
+		
+		if tilemap.get_cell_source_id(0, cell) == -1:
+			position_ok = true
+			new_pos = Vector2(x, y) * cell_size
+			
 	current_food.position = new_pos
 	add_child(current_food)
-
+	
 func food_collision():
 	if head.get_grid_pos() == current_food.get_grid_pos():
 		spawn_food()
