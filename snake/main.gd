747bf7d67ccc0677ebfca61b7c4da0788daf8153
GMarinP <giovannamarinpereira@gmail.com>
2026-05-17 22:20:25 -0600

    fixed food. tried adding growth but didnt work.

--- snake/main.gd
+++ snake/main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var FoodScene = preload("res://food.tscn")
+var TailScene = preload("res://snake_body.tscn")
+var tail_segments = []
+var previous_positions = []
 var current_food
 var grid_width = 24
 var grid_height = 14
@@ -11,6 +14,10 @@ var cell_size = 48
 
 func _ready():
 	spawn_food()
+	
+func _physics_process(delta):
+	food_collision()
+	update_tail()
 
 func spawn_food():
 	if current_food:
@@ -34,5 +41,30 @@ func spawn_food():
 	add_child(current_food)
 	
 func food_collision():
-	if head.get_grid_pos() == current_food.get_grid_pos():
+	if head.global_position.distance_to(current_food.global_position) < 24:
 		spawn_food()
+		grow_tail()
+		
+func update_tail():
+	# Record the head's position each frame
+	previous_positions.push_front(head.global_position)
+
+	# Keep only enough positions for the tail length
+	var needed = tail_segments.size() + 1
+	if previous_positions.size() > needed:
+		previous_positions.resize(needed)
+
+	# Move each tail segment to the position the head used to be in
+	for i in range(tail_segments.size()):
+		tail_segments[i].global_position = previous_positions[i + 1]
+
+func grow_tail():
+	var segment = TailScene.instantiate()
+	add_child(segment)
+
+	# Place it at the last known tail position
+	if tail_segments.size() == 0:
+		segment.global_position = head.global_position
+	else:
+		segment.global_position = tail_segments[-1].global_position
+		tail_segments.append(segment)
