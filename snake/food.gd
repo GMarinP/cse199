8c8ef512d6a74c13ebc3b03408c4ef78407f90a5
GMarinP <giovannamarinpereira@gmail.com>
2026-05-19 13:45:24 -0600

    divided everything into its own script. it is broken now, but it should work

--- snake/food.gd
+++ snake/food.gd
@@ -1,8 +1,47 @@
 extends Area2D
 # Called when the node enters the scene tree for the first time.
+
+var food_count = 0
+var HeadScript = load("res://snake_head.gd")
+var HeadNode = load("res://snake_head.tscn")
+var SnakeBody = load("res://snake_body.gd")
+var Background = load("res://muralha.tscn")
+var current_food 
+var grid_width = 24
+var grid_height = 14
+var cell_size = 48
+
 func _ready() -> void:
 	randomize()
+	spawn_food()
 
 func get_grid_pos():
 	return Vector2(round(position.x / 48), round(position.y / 48))
 	
+func _physics_process(delta):
+	food_collision()
+
+func spawn_food():
+	if current_food:
+		current_food.queue_free()
+	
+	var position_ok = false
+	var new_pos = Vector2.ZERO
+	
+	while not position_ok:
+		var x = randi() % grid_width
+		var y = randi() % grid_height
+		var cell = Vector2i(x, y)
+		
+		if Background.get_cell_source_id(cell) == -1:
+			position_ok = true
+			new_pos = Vector2(x, y) * cell_size
+			
+	current_food.position = new_pos
+	add_child(current_food)
+	
+func food_collision():
+	if HeadNode.global_position.distance_to(current_food.global_position) < 24:
+		food_count += 1
+		spawn_food()
+		#SnakeBody.grow_tail()
