feecbcb36e4f0b76d6bc0776d5db65fe9c22ae03
GMarinP <giovannamarinpereira@gmail.com>
2026-05-26 21:57:41 -0600

    snake body is in progress

--- snake/food.gd
+++ snake/food.gd
@@ -18,30 +18,27 @@ func _ready() -> void:
 
 func get_grid_pos():
 	return Vector2(round(position.x / 48), round(position.y / 48))
-	
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	food_collision()
 
 func spawn_food():
+# this function checks for cells that are not walls and spawns food at random spots.
 	if not current_food:
 		current_food = self
-		
-	
 	var position_ok = false
 	var new_pos = Vector2.ZERO
-	
 	while not position_ok:
 		var x = randi() % grid_width
 		var y = randi() % grid_height
 		var cell = Vector2i(x, y)
-		
 		if Background.get_cell_source_id(cell) == -1:
 			position_ok = true
 			new_pos = Vector2(x, y) * cell_size
-			
 	current_food.position = new_pos
-	
+
 func food_collision():
+# this function handles food collision with the snake's head 
 	if HeadNode.global_position.distance_to(current_food.global_position) < cell_size:
 		food_count += 1
 		print(food_count)
--- snake/snake_body.gd
+++ snake/snake_body.gd
@@ -1,31 +1,26 @@
 extends CharacterBody2D
-#
-#
-#
+
+var previous_positions: Array = []
+var tail_segments: Array = []
+var TailScene = preload("res://snake_body.tscn")
+@onready var head = get_node("/root/TileMap/snakeHead")
+
 func _ready() -> void:
 	pass
-	
-#
-#func update_tail():
-	## Record the head's position each frame
-	#previous_positions.push_front(head.global_position)
-#
-	## Keep only enough positions for the tail length
-	#var needed = tail_segments.size() + 1
-	#if previous_positions.size() > needed:
-		#previous_positions.resize(needed)
-#
-	## Move each tail segment to the position the head used to be in
-	#for i in range(tail_segments.size()):
-		#tail_segments[i].global_position = previous_positions[i + 1]
-#
-#func grow_tail():
-	#var segment = TailScene.instantiate()
-	#add_child(segment)
-#
-	## Place it at the last known tail position
-	#if tail_segments.size() == 0:
-		#segment.global_position = head.global_position
-	#else:
-		#segment.global_position = tail_segments[-1].global_position
-		#tail_segments.append(segment)
+
+func update_tail():
+	previous_positions.push_front(head.global_position)
+	var needed = tail_segments.size() + 1
+	if previous_positions.size() > needed:
+		previous_positions.resize(needed)
+	for i in range(tail_segments.size()):
+		tail_segments[i].global_position = previous_positions[i + 1]
+
+func grow_tail():
+	var segment = TailScene.instantiate()
+	add_child(segment)
+	if tail_segments.size() == 0:
+		segment.global_position = head.global_position
+	else:
+		segment.global_position = tail_segments[-1].global_position
+		tail_segments.append(segment)
