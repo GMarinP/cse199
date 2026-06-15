5a0abcc393de47dc5a95b6afa1995c26fec927b8
Giovanna Marin Pereira <giovannamarinpereira@gmail.com>
2026-06-14 19:48:17 -0600

    tried new movement code. doesnt work

--- snake/snake_body.gd
+++ snake/snake_body.gd
@@ -1,23 +1,11 @@
-extends CharacterBody2D
+extends StaticBody2D
 
-var TailScene = preload("res://snake_body.tscn")
-var head: CharacterBody2D
+
 var parent: Node2D
+var last_frame: Vector2
 
 func _physics_process(_delta: float) -> void:
-	update_tail()
-
-func set_up():
-	var offset = head.direction * -48 
-	if head.tail_segments.size() == 0:
-		head.segment.global_position = head.last_tile + offset
-	else:
-		head.segment.global_position = head.tail_segments[-1].global_position + offset
-
-func update_tail():
-	head.previous_positions.push_front(head.position)
-	var needed = head.tail_segments.size() + 1
-	if head.previous_positions.size() > needed:
-		head.previous_positions.resize(needed)
-	for i in range(head.tail_segments.size()):
-		head.tail_segments[i].global_position = head.previous_positions[i + 1]
+	if Input.is_action_pressed("ui_text_delete"):
+		return
+	last_frame = global_position
+	global_position = parent.last_frame
--- snake/snake_move.gd
+++ snake/snake_move.gd
@@ -10,34 +10,33 @@ var speed: int = 4
 const BODY_SEGMENT: PackedScene = preload("res://snake_body.tscn")
 var segments: Array[StaticBody2D]
 
-func _ready() -> void:
-	pass
-	
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("ui_text_delete"):
 		return
-		var input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if input and (input.x == 0 or input.y == 0) and ((input.x and not direction.x) or (input.y and not direction.y)):
 		input_dir = input
-		last_frame = global_position
+	last_frame = global_position
 	global_position += direction * speed
 	if global_position.snapped(Vector2(3,3)) == global_position.snapped(Vector2(48,48)):
-			update()
+		update()
 	if ray.is_colliding():
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("ui_accept"):
+		score += 1
+		print(score)
 		@warning_ignore("integer_division")
 		for i in range(24 / speed + 1):
 			await get_tree().process_frame
 			add_segment()
-	
+
 func add_segment():
 	var new_segment: StaticBody2D = BODY_SEGMENT.instantiate()
 	if segments.size() == 0:
 		new_segment.parent = self
 	else:
 		new_segment.parent = segments[-1]
-		segments.append(new_segment)
+	segments.append(new_segment)
 	add_sibling(new_segment)
 	new_segment.global_position = new_segment.parent.global_position
 
@@ -49,11 +48,11 @@ func update() -> void:
 		global_position = global_position.snapped(Vector2(48,48))
 		input_dir = Vector2.ZERO
 		
-func _on_area_2d_area_entered(_area: Area2D) -> void:
-	#when the head touches fruit this function instantiates a tail segment
-	segment = TailScene.instantiate()
-	await get_tree().physics_frame
-	segment.head = self
-	add_sibling(segment)
-	tail_segments.append(segment)
-	segment.set_up()
+#func _on_area_2d_area_entered(_area: Area2D) -> void:
+	##when the head touches fruit this function instantiates a tail segment
+	#segment = TailScene.instantiate()
+	#await get_tree().physics_frame
+	#segment.head = self
+	#add_sibling(segment)
+	#tail_segments.append(segment)
+	#segment.set_up()
